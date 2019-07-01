ee2ea725556ab986e2f122d9a1745c08d16c8a0d
Paraic Cooney <paraiccooney@hotmail.com>
2019-07-01 10:39:06 +0000

    flash functionality added

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,21 +1,9 @@
-function greenFlash(){
-    $(".green-button").animate({backgroundColor: white,
-        backgroundImage: radialGradient(circle at 100% 100%, grey 150px, white 15px)},2000,"fast");
-}
-
-
-
 $(document).ready(function(){
 $(".simon-button").on("click", function(){
     var color = this.id;
-    color+"Flash()";
+    $(this).addClass(color+"Flash");
+    setTimeout(function (){$(".simon-button").removeClass(color+"Flash");}, animationLength)
     });
-    //$(this).addClass(color+"Flash");
-    //setTimeout(function (){$(".simon-button").removeClass(color+"Flash");}, animationLength)
 })
 
-/* function removeClass(thisButton) {
-    thisButton.classList.remove('orange');
-} */
-
-var animationLength = 200
+var animationLength = 400;
